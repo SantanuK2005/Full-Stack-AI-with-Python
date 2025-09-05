f6d904207277a62e865916b58da7290258bff7c0
Santanu Khorat <santanukhorat@gmail.com>
2025-09-06 00:51:14 +0530

    loops Completed

--- 04_loops/09_walrus.py
+++ 04_loops/09_walrus.py
@@ -4,3 +4,26 @@
 
 # if remainder:
     # print(f"Not divisible by 5, remainder is {remainder}")
+
+value = 13
+if remainder := value % 5:
+    print(f"Not divisible by 5, remainder is {remainder}")
+    
+
+# available_sizes = ["small", "medium", "large"]
+
+# if (requested_size := input("Enter a size: ")) in available_sizes:
+#     print(f"Serving {requested_size} Chai")
+
+# else:
+#    print(f"size is not available-{requested_size}")
+
+
+
+flavors = ["Masala Tea", "Ginger Tea", "Cardamom Tea", "Saffron Tea"]
+
+print("Available flavors:", flavors)
+
+while (flavor := input("Choose a flavor: ")) not in flavors:
+    print(f"Sorry, {flavor} is not available")
+print(f"You chose {flavor} flavor")

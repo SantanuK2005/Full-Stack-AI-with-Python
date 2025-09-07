bba0191b0e7e83668207a262bda8449c215e1e5f
Santanu Khorat <santanukhorat@gmail.com>
2025-09-08 00:21:58 +0530

    function

--- 05_functions/01_duplication.py
+++ 05_functions/01_duplication.py
@@ -1,5 +1,6 @@
-def print_order():
-    pass
+def print_order(name, chai_type):
+    print(f":{name},Order received {chai_type}")
 
-print_order()
-print("Order received")
+print("santanu", "masala chai")
+print("Hitesh", "ginger chai")
+print("Ankit", "lemon chai")
---
+++ 05_functions/03_hiding.py
@@ -0,0 +1,16 @@
+def get_input():
+    print("Enter your input:")
+
+def valid_input():
+    print("Validating the input")
+
+def sav_to_db():
+    print("Saving to the database")
+
+def register_user():
+    get_input()
+    valid_input()
+    sav_to_db()
+    print("User registration complete")
+
+register_user()

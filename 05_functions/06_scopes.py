1748c0c190e3c00522768415ec39e28a1c4f4c03
Santanu Khorat <santanukhorat@gmail.com>
2025-09-13 00:26:17 +0530

    function

--- 05_functions/06_scopes.py
+++ 05_functions/06_scopes.py
@@ -1,3 +1,20 @@
 def serve_chai():
     chai_type = "Masala"
-    print(f"Instand function")
+    print(f"Instand function {chai_type}") 
+
+
+chai_type = "lemon"
+serve_chai()
+print(f"outside function: {chai_type}")
+
+def chai_counter():
+    chai_order = "Lemon"
+    def print_order():
+        chai_order = "Ginger"
+        print("Inner:", chai_orde)
+    print_order()
+    print("order:", chai_order)
+
+chai_orer = "tulsi"
+chai_counter()
+print("Global :", chai_order)
